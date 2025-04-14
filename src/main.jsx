c722c077f6db0d78a782f50e6a2b90d45f03d762
Al-AminWeb <143194944+Al-AminWeb@users.noreply.github.com>
2025-04-15 01:11:41 +0600

    Understand The Concept of Routing

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,27 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router'
+import Root from './components/Root/Root.jsx'
+import Home from './components/Home/Home.jsx'
+import Mobile from "./components/Mobile/Mobile.jsx";
+import Laptops from "./components/Laptops/Laptops.jsx";
+
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        Component: Root,
+        children: [
+            {index: true, Component: Home},
+            {path: 'mobile', Component: Mobile},
+            {path: 'laptops', Component: Laptops}
+        ]
+    },
+])
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+    <StrictMode>
+        <RouterProvider router={router}></RouterProvider>
+    </StrictMode>,
 )
